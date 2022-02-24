107dcb601a9a4c8e47cf2dac7658bcf77e9e95b0
Some One <adrian97.z@outlook.com>
2022-02-24 10:57:05 +0100

    Dodaj pliki projektów.

---
+++ static elements/static elements.cpp
@@ -0,0 +1,54 @@
+﻿
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+class Car;
+
+class Serwis {
+private:
+    float cena_naprawy;
+public:
+    void ileSerwis(Car& obiekt, float naprawa);
+};
+
+class Car {
+private:
+    string marka;
+    string model;
+    float spalanie;
+public:
+    Car(string m_marka, string m_model, float s_spalanie)
+        : marka(m_marka), model(m_model), spalanie(s_spalanie){}
+    
+    void gettingData()
+    {
+        cout << marka << endl;
+        cout << model << endl;
+        cout << spalanie << endl;
+    }
+
+    void operator + ()
+    {
+        model += 1;
+    }
+
+    friend void Serwis::ileSerwis(Car& obiekt, float naprawa);
+};
+
+void Serwis::ileSerwis(Car& obiekt, float naprawa)
+{
+    cout << "Za auto marki " << obiekt.marka << " trzeba zaplacic " << naprawa << endl;
+}
+
+int main()
+{
+    Car c1("Volvo", "V40", 7.5);
+
+    Serwis s1;
+
+    s1.ileSerwis(c1, 200);
+}
+
